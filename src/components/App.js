fb57c1df74747d8fb7350f0238de1206ff5c9223
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-22 12:13:56 +0530

    feat: implement counter app with Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import './../styles/App.css';
 
 const App = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>{count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
